b1a7423eb41eb79986247cd8efbb40c5c671362a
Nick <nick@lita.foundation>
2024-07-08 16:10:23 -0400

    Updated risc0 fibonacci to use v1.0.1 (and above)

--- fibonacci/risc0/methods/build.rs
+++ fibonacci/risc0/methods/build.rs
@@ -1,3 +1,13 @@
 fn main() {
+    // Rerun if any file in the methods/guest/src directory changes
+    println!("cargo:rerun-if-changed=guest/src");
+    
+    // Rerun if the Cargo.toml file changes
+    println!("cargo:rerun-if-changed=Cargo.toml");
+    
+    // Rerun if this build script changes
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Call the risc0 build function
     risc0_build::embed_methods();
 }
